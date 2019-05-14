c9da80bafa03ed4b0b1f405ba6863ead5e942baf
eduardonunes <eduardonunes2525@gmail.com>
2019-05-14 17:36:00 -0300

    Adds shader modules

---
+++ triangle/compile.sh
@@ -0,0 +1,2 @@
+/home/eduardonunes/Documentos/instalations/vulkan1.1.101.0/x86_64/bin/glslangValidator -V shaders/shader.vert
+/home/eduardonunes/Documentos/instalations/vulkan1.1.101.0/x86_64/bin/glslangValidator -V shaders/shader.frag
